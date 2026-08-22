d6fba1f7206515303acabc6a9c6e72bc960fcf83
mithunsahoo <77102407+mithunsahoo@users.noreply.github.com>
2026-08-22 14:36:29 +0530

    Update FindMissingNo.java

--- TestOnline/src/org/socialMedia/FindMissingNo.java
+++ TestOnline/src/org/socialMedia/FindMissingNo.java
@@ -1,14 +1,23 @@
-class FindMissingNo {
+class Main {
     public static void main(String[] args) {
 
-        int[] n = {2, 6, 8};
+        int[] n = {8, 2, 6};
 
+        // Bubble sort
         for (int i = 0; i < n.length - 1; i++) {
-            int next = n[i] + 1;
+            for (int j = 0; j < n.length - 1 - i; j++) {
+                if (n[j] > n[j + 1]) {
+                    int temp = n[j];
+                    n[j] = n[j + 1];
+                    n[j + 1] = temp;
+                }
+            }
+        }
 
-            while (next < n[i + 1]) {
-                System.out.println(next);
-                next++;
+        // Find missing numbers
+        for (int i = 0; i < n.length - 1; i++) {
+            for (int k = n[i] + 1; k < n[i + 1]; k++) {
+                System.out.println(k);
             }
         }
     }
