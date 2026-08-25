7ccd1c05527434d551838f456d051bab54670c42
mithunsahoo <77102407+mithunsahoo@users.noreply.github.com>
2026-08-25 18:15:56 +0530

    Update AngramCheck.java

--- TestOnline/src/org/socialMedia/AngramCheck.java
+++ TestOnline/src/org/socialMedia/AngramCheck.java
@@ -17,3 +17,37 @@ class AngramCheck {
             System.out.println("Not Anagram");
     }
 }
+
+/* 
+Manual Sorting
+
+    class Main {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        char[] a = str1.toCharArray();
+        char[] b = str2.toCharArray();
+
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    char temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+
+                if (b[i] > b[j]) {
+                    char temp = b[i];
+                    b[i] = b[j];
+                    b[j] = temp;
+                }
+            }
+        }
+
+        if (new String(a).equals(new String(b)))
+            System.out.println("Anagram");
+        else
+            System.out.println("Not Anagram");
+    }
+}  */
